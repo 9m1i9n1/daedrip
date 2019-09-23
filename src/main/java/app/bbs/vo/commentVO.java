afea1988d67c69f024b4f6bdec94c2827aa9d453
KIM <koko0869@naver.com>
2019-09-23 21:36:25 +0900

    dd

--- src/main/java/app/bbs/vo/commentVO.java
+++ src/main/java/app/bbs/vo/commentVO.java
@@ -5,11 +5,12 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class commentVO {
-  private int idx;
-  private int bbs_idx;
+public class CommentVO {
+  private int idx; // cno
+  private int bbs_idx; // bno
   private int account_idx;
   private String content;
-  private Date regDate;
+  private Date regDate;// reg_date
   private Date modifyDate;
+  // private String Writer;
 }
