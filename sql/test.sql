7c4ac6c52a86d352dfd4127c65e81f0b9a31e7e2
hyun <zcvfcat@gmail.com>
2019-09-26 13:27:38 +0900

    v

--- sql/test.sql
+++ sql/test.sql
@@ -1,3 +1,4 @@
 select * from account where userid=3333;
 
 select * from account where userid="" and pw="";
+
--- src/main/java/app/config/CertificationInterceptor.java
+++ src/main/java/app/config/CertificationInterceptor.java
@@ -1,16 +1,38 @@
 package app.config;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import app.sign.service.SignService;
+import app.sign.vo.SignVO;
 
 @Component
 public class CertificationInterceptor implements HandlerInterceptor {
+
+  @Autowired
+  SignService signService;
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-    
-    return HandlerInterceptor.super.preHandle(request, response, handler);
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+      ModelAndView modelAndView) throws Exception {
+    HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+      throws Exception {
+    HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
   }
 }
