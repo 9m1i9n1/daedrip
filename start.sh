8c4802c2dd02033417b417cc7ef61a467ab4da1b
min.dev <9m1i9n1@gmail.com>
2019-09-28 20:51:20 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,25 @@
-appName="p1"
+appName="9m1i9n1/p1"
+containerName="p1"
 word="1"
 echo "$word"
-word=`docker ps -a -q --no-trunc --filter name=^/"$appName"$`
+word=`docker ps -a -q --no-trunc --filter name=^/"$containerName"$`
 echo "$word"
 if [ -z "$word" ];
 then
-    echo "해당 컨테이너가 존재하지 않아 실행-------------------------------------"
+    echo "해당 컨테이너가 존재하지 않아 실행------------------"
 elif [ -n "$word" ];
 then
-    echo "해당 컨테이너가 존재하여 정지시키고 삭제--------------------------------"
+    echo "해당 컨테이너가 존재하여 정지시키고 삭제------------"
     /usr/bin/docker stop "$word"
     /usr/bin/docker rm "$word"
 elif [ "$word" == "1" ];
 then
-    echo "컨테이너 에러로 종료하고 삭제------------------------------------------"
+    echo "컨테이너 에러로 종료하고 삭제----------------------"
     /usr/bin/docker stop "$word"
     /usr/bin/docker rm "$word"
 fi
-/home/centos/apache-maven-3.6.2/bin mvn clean package -P spring-boot docker:build
-/usr/bin/docker run -p 18080:18080 -d --name "$appName" "$appName":latest
+
+/usr/bin/docker push "$appName"
+/usr/bin/docker run -p 28080:28080 -d --name "$containerName" "$appName":latest
+
+echo "완료------------------------------------------------"
